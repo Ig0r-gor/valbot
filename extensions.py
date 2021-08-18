8a0ca1c01bc53dd2c430f0c192ebef342da867c8
IGor <79050023+Ig0r-gor@users.noreply.github.com>
2021-08-18 21:23:19 +0400

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -7,14 +7,9 @@ class APIException(Exception):
 
 class MoneyConverter:
     @staticmethod
-    def get_price(quote: str, base: str, amount: str):
+    def get_price(base: str, quote: str, amount: str):
         if quote == base:
             raise APIException(f'Перевод одинаковых валют не выполняется: {base} в {quote}')
-
-        try:
-            quote_ticker = money[quote]
-        except KeyError:
-            raise APIException(f'Не удалось обработать значение: {quote}')
 
         try:
             base_ticker = money[base]
@@ -22,11 +17,16 @@ class MoneyConverter:
             raise APIException(f'Не удалось обработать значение: {base}')
 
         try:
+            quote_ticker = money[quote]
+        except KeyError:
+            raise APIException(f'Не удалось обработать значение: {quote}')
+
+        try:
             amount = float(amount)
         except ValueError:
             raise APIException(f'Не удалось обработать количество:  {amount}')
 
-        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
-        total_base = (json.loads(r.content)[money[base]])*amount
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={base_ticker}&tsyms={quote_ticker}')
+        total_base = (json.loads(r.content)[money[quote]])*amount
 
         return total_base
